8851c9e0531d8eb9a1f43c699462decbb8daacc6
enkog <nkirukaogu@gmail.com>
2021-08-31 10:52:15 +0100

    Add basic styles to the layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+
+const scoresArr = [
+  { name: 'Nkiruka', score: 50 },
+  { name: 'John', score: 150 },
+  { name: 'Benjamin', score: 250 },
+  { name: 'Daniel', score: 350 },
+  { name: 'Seun', score: 60 },
+  { name: 'James', score: 30 },
+  { name: 'Arthur', score: 70 },
+  { name: 'Daniella', score: 400 },
+];
+
+const renderList = (arr) => {
+  const ul = document.querySelector('.scores-listing');
+
+  arr.forEach((e) => {
+    const li = document.createElement('li');
+    li.className = 'list-item p-2';
+    li.textContent = `${e.name}: ${e.score}`;
+    ul.appendChild(li);
+  });
+};
+
+renderList(scoresArr);
