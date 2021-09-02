d4d7b20002db957db1abe814abf4ece30f02bce4
enkog <nkirukaogu@gmail.com>
2021-09-02 12:53:46 +0100

    Modify function to render a list to use the leaderboard api and extract it to a module

---
+++ src/renderList.js
@@ -0,0 +1,13 @@
+async function renderList() {
+  const ul = document.querySelector('.scores-listing');
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/AxewAR6mMlFD5bRkqxdb/scores';
+  const response = await fetch(url);
+  const scores = await response.json();
+
+  scores.result.forEach((e) => {
+    const li = document.createElement('li');
+    li.className = 'list-item p-2';
+    li.textContent = `${e.user}: ${e.score}`;
+    ul.appendChild(li);
+  });
+}
