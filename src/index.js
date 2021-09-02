d3d4e97cbc2c741935b47a9be156717a63f6bf21
enkog <nkirukaogu@gmail.com>
2021-09-02 13:03:49 +0100

    Implement the submit button to send data to the API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
 import renderList from './renderList.js';
 import addScore from './addScore.js';
+
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener('click', () => {
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  const data = { user: `${name}`, score: parseInt(`${score}`, 10) };
+  addScore(data);
+});
